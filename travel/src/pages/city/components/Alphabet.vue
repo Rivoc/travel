b7cec25aa3a5a3211db43a69a8412e6c9d450c6f
Rivoc <1074852904@qq.com>
2019-09-11 21:55:40 +0800

    add city css

---
+++ travel/src/pages/city/components/Alphabet.vue
@@ -0,0 +1,38 @@
+<template>
+  <ul class="list">
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+  </ul>
+</template>
+<script>
+export default {
+  name: 'CityAlphabet'
+}
+</script>
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl';
+@import '~styles/mixins.styl';
+
+.list {
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  top: 1.5rem;
+  right: 0;
+  bottom: 0;
+  color: $bgColor;
+  font-size: 0.18rem;
+  width: 0.3rem;
+
+  .item {
+    margin-bottom: 0.1rem;
+  }
+}
+</style>
---
+++ travel/src/pages/city/components/List.vue
@@ -0,0 +1,174 @@
+<template>
+  <div class="list"
+       ref="wrapper">
+    <div>
+      <div class="area">
+        <h2 class="title">当前城市</h2>
+        <div class="button-list">
+          <div class="button-wrapper">
+            <div class="button">长沙</div>
+          </div>
+        </div>
+      </div>
+      <div class="area">
+        <h2 class="title">热门城市</h2>
+        <div class="button-list">
+          <div class="button-wrapper">
+            <div class="button">北京</div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">北京</div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">北京</div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">北京</div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">北京</div>
+          </div>
+        </div>
+      </div>
+      <div class="area">
+        <h2 class="title border-topbottom">A</h2>
+        <div class="item-list">
+          <div class="item border-bottom">
+            阿拉尔
+          </div>
+          <div class="item border-bottom">
+            阿拉尔
+          </div>
+          <div class="item border-bottom">
+            阿拉尔
+          </div>
+          <div class="item border-bottom">
+            阿拉尔
+          </div>
+          <div class="item border-bottom">
+            阿拉尔
+          </div>
+          <div class="item border-bottom">
+            阿拉尔
+          </div>
+          <div class="item border-bottom">
+            阿拉尔
+          </div>
+          <div class="item border-bottom">
+            阿拉尔
+          </div>
+          <div class="item border-bottom">
+            阿拉尔
+          </div>
+          <div class="item border-bottom">
+            阿拉尔
+          </div>
+        </div>
+        <h2 class="title border-topbottom">A</h2>
+        <div class="item-list">
+          <div class="item border-bottom">
+            阿拉尔
+          </div>
+          <div class="item border-bottom">
+            阿拉尔
+          </div>
+          <div class="item border-bottom">
+            阿拉尔
+          </div>
+          <div class="item border-bottom">
+            阿拉尔
+          </div>
+          <div class="item border-bottom">
+            阿拉尔
+          </div>
+          <div class="item border-bottom">
+            阿拉尔
+          </div>
+          <div class="item border-bottom">
+            阿拉尔
+          </div>
+          <div class="item border-bottom">
+            阿拉尔
+          </div>
+          <div class="item border-bottom">
+            阿拉尔
+          </div>
+          <div class="item border-bottom">
+            阿拉尔
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import Bscroll from 'better-scroll'
+export default {
+  name: 'CityList',
+  mounted () {
+    console.log(this.$refs.wrapper)
+    this.scroll = new Bscroll(this.$refs.wrapper)
+  }
+}
+</script>
+<style lang="stylus" scoped>
+.border-topbottom {
+  &:before {
+    color: #e7e7e7;
+
+    &:after {
+      color: #e7e7e7;
+    }
+  }
+}
+
+.list {
+  position: absolute;
+  top: 0.725rem;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background: #f5f5f5;
+  overflow: hidden;
+
+  .title {
+    font-size: 0.12rem;
+    padding: 0.12rem 0.15rem;
+  }
+
+  .button-list {
+    display: flex;
+    flex-wrap: wrap;
+    padding: 0.2rem 0.3rem 0 0.3rem;
+    background: #fff;
+
+    .button-wrapper {
+      width: 2rem;
+      border: 1px solid #dedede;
+      text-align: center;
+      margin-right: 0.2rem;
+      margin-bottom: 0.2rem;
+
+      .button {
+        font-size: 0.14rem;
+        line-height: 0.28rem;
+      }
+    }
+
+    .button-wrapper:nth-of-type(3n) {
+      margin-right: 0;
+    }
+  }
+
+  .item-list {
+    background: #fff;
+    line-height: 0.4rem;
+    font-size: 0.14rem;
+
+    .item {
+      padding-left: 0.15rem;
+      padding-right: 0.6rem;
+    }
+  }
+}
+</style>
