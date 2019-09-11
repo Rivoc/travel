2b62d6392cd30b9c857aac05bf217928febb92f9
Rivoc <1074852904@qq.com>
2019-09-11 17:12:14 +0800

    add search to city

---
+++ travel/src/pages/city/components/Search.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="search">
+    <input class="input-search"
+           type="text"
+           placeholder="请输入城市名">
+  </div>
+</template>
+<script>
+export default {
+  name: 'HomeSearch'
+}
+</script>
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl';
+
+.search {
+  padding: 0.1rem;
+  padding-top: 0;
+  background: $bgColor;
+
+  height, .input-search {
+    width: 100%;
+    padding: 0 0.03rem;
+    box-sizing: border-box;
+    font-size: 0.14rem;
+    border-radius: 0.05rem;
+    color: #666;
+  }
+}
+</style>
