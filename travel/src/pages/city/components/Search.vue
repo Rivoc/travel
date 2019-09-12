e740835f8868402b3c49650db27232319ec219d2
Rivoc <1074852904@qq.com>
2019-09-12 17:08:55 +0800

    search finish

--- travel/src/pages/city/components/Search.vue
+++ travel/src/pages/city/components/Search.vue
@@ -1,14 +1,73 @@
 <template>
-  <div class="search">
-    <input class="input-search"
-           type="text"
-           placeholder="请输入城市名">
+  <div>
+    <div class="search">
+      <input class="input-search"
+             type="text"
+             placeholder="请输入城市名或拼音"
+             v-model="keyword">
+    </div>
+    <div class="search-content"
+         :ref="wrapper"
+         v-show="hasKeyWord">
+      <ul>
+        <li class="search-item border-bottom"
+            v-for="item of list"
+            :key=item.id>{{item.name}}</li>
+        <li class="search-item border-bottom"
+            v-show="hasNodata">没有找到匹配数据</li>
+      </ul>
+    </div>
   </div>
 </template>
 <script>
+import Bscroll from 'better-scroll'
 export default {
-  name: 'HomeSearch'
+  name: 'HomeSearch',
+  props: {
+    cities: Object
+  },
+  data () {
+    return {
+      keyword: '',
+      list: [],
+      timer: null
+    }
+  },
+  computed: {
+    hasNodata () {
+      return !this.list.length
+    },
+    hasKeyWord () {
+      return this.keyword
+    }
+  },
+  mounted () {
+    this.scroll = new Bscroll(this.$refs.wrapper)
+  },
+  watch: {
+    keyword () {
+      if (this.timer) {
+        clearTimeout(this.timer)
+      }
+      if (!this.keyword) { // 搜索之后删掉关键词，清掉搜索结果
+        this.list = []
+        return
+      }
+      this.timer = setTimeout(() => {
+        const result = []
+        for (let i in this.cities) {
+          this.cities[i].forEach((value) => {
+            if (value.spell.indexOf(this.keyword) > -1 || value.name.indexOf(this.keyword) > -1) {
+              result.push(value)
+            }
+          })
+        }
+        this.list = result
+      }, 100)
+    }
+  }
 }
+
 </script>
 <style lang="stylus" scoped>
 @import '~styles/varibles.styl';
@@ -25,6 +84,22 @@ export default {
     font-size: 0.14rem;
     border-radius: 0.05rem;
     color: #666;
+  }
+}
+
+.search-content {
+  position: absolute;
+  z-index: 1;
+  top: 0.72rem;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background: #fff;
+  font-size: 0.14rem;
+
+  .search-item {
+    line-height: 0.28rem;
+    padding-left: 0.2rem;
   }
 }
 </style>
